5ed448f3a1bd8983165a0e731b63b0b3b33c6cc4
NatalyaBortsova <natanatka31@gmail.com>
2022-05-26 21:22:16 +0300

    fix path

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  basePath: "/portfolio-nextjs",
-  assetPrefix: "/portfolio-nextjs",
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
 
 module.exports = nextConfig
